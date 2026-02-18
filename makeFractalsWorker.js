768c5c06651e995f74cbb65988e3029a9562873b
FredericLatremoliere <frederic.latremoliere@outlook.com>
2026-02-17 19:32:16 -0700

    Add files via upload

--- makeFractalsWorker.js
+++ makeFractalsWorker.js
@@ -1,18 +1,55 @@
+class Complex
+{
+    constructor (x,y) { this.set(x,y); };
+    set(x,y) {  this.re = x; this.im = y; return this; };
+
+    
+    add(t1,t2) { return this.set(t1.re + t2.re, t1.im + t2.im); }
+    sub(t1,t2) { return this.set(t1.re - t2.re, t1.im - t2.im); }
+    conj(z)    { return this.set(z.re,-ze.im); }
+    mul(f1,f2) { let x1 = f1.re; let x2 = f2.re;
+		 return this.set( x1 * x2 - f1.im * f2.im, x1*f2.im + f1.im*x2) }
+    get_mod2()  { return this.re*this.re + this.im*this.im; }
+    get_mod()   { return Math.sqrt(this.get_mod2()); }
+    mod2(z)     { return this.set(z.get_mod2(),0.0); }
+    mod(z)      { return this.set(z.get_mod(), 0.0); }
+
+    
+    inv(z)      { let m = z.get_mod2();
+		  return this.set(z.re/m, -z.im / m); }
+    div(f1,f2)  { let m = z.get_mod2();
+		  let x1 = f1.re;
+		  let x2= f1.re;
+		  return this.set((x1 * x2 - f1.im*f2.im)/m, (-x1 * f2.im - f1.im * x2)/m); }
+    
+    toPolar()   { return { r: this.get_mod(), theta: 2 * Math.atan(y / (x + r) ) }; }
+    fromPolar(r,theta) { this.re = r * Math.cos(theta); this.im = r*Math.sin(theta); return this; }
+
+    powInt(z,n) { this.re = 1;
+		  this.im = 0;
+		  for(let j = 0 ; j < n ; ++j)
+		  { this.mul(this,z); }
+		  return this; }
+
+};
 
 
 onmessage = e =>
 {
     let that=e.data;
+    that.func = new Function('z','c',`let y = new Complex(0,0); return ${that.fn};`);
+    let img = [];
     if(that.mandelbrot)
     {
-	that.colorizeCollection(makeMandelbrotSet(that) );
+	img = makeMandelbrotSet(that);
     }
     else
     {
-	that.colorizeCollection(makeJuliaSet(that));
+	img = makeJuliaSet(that);
     }
 
-    postMessage();
+    
+    postMessage(img);
 }
 
 
@@ -20,7 +57,7 @@ onmessage = e =>
 function makeMandelbrotSet(that)
 {
     let img = [ ] ;
-    let fn = that.fn ;
+    let fn = that.func ;
     
     let xstep = (that.xmax-that.xmin)/that.width ;
     let ystep = (that.ymax-that.ymin)/that.height ;
@@ -47,7 +84,7 @@ function makeMandelbrotSet(that)
 	    
 	    let N = 0;
 	    
-	    while(z.get_mod2() < 2 && ++N < that.max_iter)
+	    while(z.get_mod2() < 2 && N++ < that.max_iter)
 	    {
 		z = fn(z,c) ; 
 	    }
@@ -60,13 +97,13 @@ function makeMandelbrotSet(that)
     return img;
 }
 
-makeJuliaSet()
+function makeJuliaSet(that)
 {
     let xstep = (that.xmax-that.xmin)/that.width ;
     let ystep = (that.ymax-that.ymin)/that.height ;
     
     let img = [ ];
-    let fn = that.fn;
+    let fn = that.func;
     
     let z = new Complex(that.xmin,that.ymax) ;
     let c = that.c;
@@ -87,7 +124,7 @@ makeJuliaSet()
 	    
 	    let N = 0;
 	    
-	    while(z.get_mod2() < 4 && ++N < that.max_iter)
+	    while(z.get_mod2() < 4 && N++ < that.max_iter)
 	    {
 		z = fn(z,c); 
 	    }
@@ -99,4 +136,3 @@ makeJuliaSet()
     }
     return img;
 }
-
